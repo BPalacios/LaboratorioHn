35edab960ffa215751ab6b4bdc531f4441f8990c
BPalacios <alex.palacios@unah.hn>
2019-05-02 22:23:49 -0600

    Modificando este relajo

--- __manifest__.py
+++ __manifest__.py
@@ -1,23 +1,19 @@
 # -*- coding: utf-8 -*-
 {
     'name': "LaboratorioHn",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Prueba',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'category': 'Prueba',
+    'author': 'Alex Palacios, Hristo Oviedo',
     'depends': ['base'],
-
-    # always loaded
+    'maintainer': 'Alex Palacios',
+    'Company':'Centro Medico Chatoyer',
+    'website':'Pendiete',
     'data': [
-        # 'security/ir.model.access.csv',
         'views/openacademy.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'images':[],
+    'license': 'AGPL-3',
+    'installable': True,
+    'application' : False,
+    'auto_install' :False,
 }
