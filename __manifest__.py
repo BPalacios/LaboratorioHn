b31481776fe198fe30f64943359a1e3313be53d5
BPalacios <alex.palacios@unah.hn>
2019-05-02 22:06:58 -0600

    de aqui en adelante

--- __manifest__.py
+++ __manifest__.py
@@ -2,16 +2,6 @@
 {
     'name': "LaboratorioHn",
 
-    'summary': """
-       El siguiente modulo permite registrar los pacientes, registrar sus examenes e imprimirlos""",
-
-    'description': """
-        El siguiente modulo administra las dependencias de un laboratorio clinico
-    """,
-
-    'author': "Alex Palacios",
-    'website': "http://www.LabChatoyer.com",
-
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
@@ -24,8 +14,6 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
         'views/openacademy.xml',
     ],
     # only loaded in demonstration mode
