2c7851f9ff917fea04925deee0368e3cbcf2a0f6
BPalacios <alex.palacios@unah.hn>
2019-05-02 23:11:33 -0600

    Clase Paciente creada

--- models/paciente.py
+++ models/paciente.py
@@ -1,1 +1,21 @@
-aqui vamos
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+class Paciente(models.Model):
+	_name = 'paciente.paciente'
+
+	pnombrePaciente = fields.Char(string='Primer Nombre', required=True)
+	snombrePaciente = fields.Char(string='Segundo Nombre')
+    papellidosPaciente = fields.Char(string='Primer Apellido', required=True)
+    sapellidosPaciente = fields.Char(string='Segundo Apellido')
+	fechaNaciemiento = fields.Date(string="Fecha de Nacimiento")
+	genero = fields.Selection([('masculino', 'Masculino'), ('femenino', 'Femenino'), ('otro', 'Otro')], string='Genero')
+	edad = fields.Integer(string='Edad')
+	foto = fields.Binary(string='Foto')
+    direccionPaciente = fields.Char(string='Direccion')
+	tipodeSangre = fields.Selection(
+    	[('A+', 'A+ve'), ('B+', 'B+ve'), ('O+', 'O+ve'), ('AB+', 'AB+ve'),
+     	('A-', 'A-ve'), ('B-', 'B-ve'), ('O-', 'O-ve'), ('AB-', 'AB-ve')],
+    	string='Grupo Sanguineo')
+	nacionalidadPaciente = fields.Many2one('res.country', string='Nacionalidad')
