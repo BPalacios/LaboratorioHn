65abeb37b63393b75468ff8c4ecaafb448a96aa9
BPalacios <alex.palacios@unah.hn>
2019-05-03 16:37:56 -0600

    Espero que funcione

--- models/paciente.py
+++ models/paciente.py
@@ -19,3 +19,12 @@ class Paciente(models.Model):
      	('A-', 'A-ve'), ('B-', 'B-ve'), ('O-', 'O-ve'), ('AB-', 'AB-ve')],
     	string='Grupo Sanguineo')
     nacionalidadPaciente = fields.Many2one('res.country', string='Nacionalidad')
+
+class myconf(models.Model):
+    _name='myconferences.myconf'
+    _description='a model for a conference'
+    confname=fields.Char('ConfName' , Required=True)
+    indexed=fields.Boolean('Indexed ?', Required=True)
+    startdate=fields.Date('Start Date', Required=True)
+    enddate=fields.Date('End Date', Required=True)    
+    fee=fields.Float('Registration Fee', Required=True)
