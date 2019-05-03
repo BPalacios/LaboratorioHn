cb1d8a5d82856b540545d0bacd85aefee0825d7d
BPalacios <alex.palacios@unah.hn>
2019-05-02 23:44:56 -0600

    Correcion de Indentacion

--- models/paciente.py
+++ models/paciente.py
@@ -3,19 +3,19 @@
 from odoo import models, fields
 
 class Paciente(models.Model):
-	_name = 'paciente.paciente'
-
-	pnombrePaciente = fields.Char(string='Primer Nombre', required=True)
-	snombrePaciente = fields.Char(string='SeWgundo Nombre', required=False)
+    _name = 'paciente.paciente'
+    
+    pnombrePaciente = fields.Char(string='Primer Nombre', required=True)
+    snombrePaciente = fields.Char(string='Segundo Nombre')
     papellidoPaciente = fields.Char(string='Primer Apellido', required=True)
     sapellidoPaciente = fields.Char(string='Segundo Apellido')
-	fechaNaciemiento = fields.Date(string="Fecha de Nacimiento")
-	genero = fields.Selection([('masculino', 'Masculino'), ('femenino', 'Femenino'), ('otro', 'Otro')], string='Genero')
-	edad = fields.Integer(string='Edad')
-	foto = fields.Binary(string='Foto')
+    fechaNaciemiento = fields.Date(string="Fecha de Nacimiento")
+    genero = fields.Selection([('masculino', 'Masculino'), ('femenino', 'Femenino'), ('otro', 'Otro')], string='Genero')
+    edad = fields.Integer(string='Edad')
+    foto = fields.Binary(string='Foto')
     direccionPaciente = fields.Char(string='Direccion')
-	tipodeSangre = fields.Selection(
+    tipodeSangre = fields.Selection(
     	[('A+', 'A+ve'), ('B+', 'B+ve'), ('O+', 'O+ve'), ('AB+', 'AB+ve'),
      	('A-', 'A-ve'), ('B-', 'B-ve'), ('O-', 'O-ve'), ('AB-', 'AB-ve')],
     	string='Grupo Sanguineo')
-	nacionalidadPaciente = fields.Many2one('res.country', string='Nacionalidad')
+    nacionalidadPaciente = fields.Many2one('res.country', string='Nacionalidad')
