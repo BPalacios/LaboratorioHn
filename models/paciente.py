172eb97e66a5e67416f6d1c720a30762875e7c11
BPalacios <alex.palacios@unah.hn>
2019-05-04 19:23:15 -0600

    Nose que onda

--- models/paciente.py
+++ models/paciente.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from datetime import datetime
 from odoo import api, exceptions, models, fields, _
 
 class Paciente(models.Model):
@@ -20,6 +20,19 @@ class Paciente(models.Model):
      	('A-', 'A-ve'), ('B-', 'B-ve'), ('O-', 'O-ve'), ('AB-', 'AB-ve')],
     	string='Grupo Sanguineo')
     nacionalidadPaciente = fields.Many2one('res.country', string='Nacionalidad')
+    ordenPaciente = fields.Many2one('paciente.orden', string='Ordenes')
+    
 
 
+class Orden(models.Model):
+    _name: "paciente.orden"
+    _description: "una orden determina que examen se hara"
+
+    fechaIngreso = fields.Date(string='Fecha Ingreso', default=datetime.today(), required=True)
+    fechaImpresion = fields.Date(string='Fecha Impresion')
+    pacienteOrden = fields.One2many('paciente.paciente', inverse_name='Ordenes', string='Paciente')
+    
+    
+    
+    
 
